7f03455d926afeaeccfe717fb0a06459053b1e02
Елисей <48096405+eles13@users.noreply.github.com>
2020-02-25 00:37:25 +0300

    sec

--- first/main.cpp
+++ first/main.cpp
@@ -3,6 +3,7 @@
 #include <complex>
 #include <cmath>
 #include <omp.h>
+#include <fstream>
 using namespace std;
 
 typedef complex<double> complexd;
@@ -32,33 +33,34 @@ complexd* f(complexd *A, int n, complexd *H, int k) {
 	complexd *B = new complexd[m];
 	#pragma omp parallel for shared(A, B, H, m, l) private(i)
 		for (i = 0; i < m; ++i)
-		if ((i & l) == 0)
-		{
+		if ((i & l) == 0){
 			B[i] =  H[0]*A[i & ~l] + H[1]*A[i | l];
 		}
-		else
-		{
+		else{
 			B[i] = H[2]*A[i & ~l] + H[3]*A[i | l];
 		}
 	return B;
 }
 
 int main(int argc, char **argv) {
-
-	if (argc < 3) {
-		cout << argv[0] << " n k" << endl;
-		return 0;
+	if (argc < 4) {
+		cout << "too few args"<< endl;
+		return -1;
 	}
-	int n, k;
+	int n, k ,i;
 	n = atoi(argv[1]);
 	k = atoi(argv[2]);
+	i = atoi(argv[3]);
+	omp_set_num_threads(i);
 	double start = omp_get_wtime();
 	complexd *A = init(n);
 	complexd H[] = {1/sqrt(2), 1/sqrt(2), 1/sqrt(2), -1/sqrt(2)};
 	complexd *B = f(A, n, H, k);
 	start = omp_get_wtime() - start;
-	char* numthr = getenv("OMP_NUM_THREADS");
-	cout << n << ' ' << k << ' ' << ((numthr != NULL) ? numthr : "1") << ' ' << start << endl;
+	ofstream fout;
+	fout.open("result.txt",ofstream::out | ofstream::app);
+	fout << n << ' ' << k << ' ' << i << ' ' << start << endl;
+	fout.close();
 	delete [] A;
 	delete [] B;
 	return 0;
