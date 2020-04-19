8ff7a1c1b021590fce4046a5df35c7506dd06ffa
Елисей <48096405+eles13@users.noreply.github.com>
2020-04-20 00:32:04 +0300

    Update main.cpp

--- first/main.cpp
+++ first/main.cpp
@@ -13,7 +13,7 @@ complexd* init(int n) {
 	complexd *A = new complexd[m];
 	double sum = 0;
 	unsigned int seed = omp_get_wtime();
-	#pragma omp parallel for firstprivate(seed) reduction(+: sum)
+	#pragma omp parallel for firstprivate(seed) reduction(+: sum) schedule(guided)
 		for (i = 0; i < m; ++i) {
 			seed+=omp_get_wtime();
 			A[i].real((rand_r(&seed) / (float) RAND_MAX) - 0.5);
@@ -31,7 +31,7 @@ complexd* init(int n) {
 complexd* f(complexd *A, int n, complexd *H, int k) {
 	unsigned long long i, m = 1 << n, l = 1 << (n - k);
 	complexd *B = new complexd[m];
-	#pragma omp parallel for
+	#pragma omp parallel for firstprivate(m, l)
 		for (i = 0; i < m; ++i){
 			if ((i & l) == 0){
 				B[i] =  H[0]*A[i & ~l] + H[1]*A[i | l];
